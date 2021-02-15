5cc618bc1058e238826ee04e151c686a83020529
JaquelineMonteiro <jaqueline.monteiro2012@gmail.com>
2021-02-15 18:08:53 -0300

    Incluindo classe Aluno

--- projeto.pos/src/model/Aluno.java
+++ projeto.pos/src/model/Aluno.java
@@ -2,6 +2,92 @@ package model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
 public class Aluno implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String sobrenome;
+	
+	@Column(nullable = true)
+	private int idade;
+	
+	@Column(nullable = false)
+	private int cpf;
+	
+	@Column(nullable = true)
+	private String email;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", idade=" + idade + ", cpf=" + cpf
+				+ ", email=" + email + "]";
+	}
+	
 
 }
